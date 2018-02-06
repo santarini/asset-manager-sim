40c5098b5a25cb8ecd0e28eb137a2001303f4e78
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-05 20:00:30 -1000

    Update buyShare.js

--- buyShare.js
+++ buyShare.js
@@ -1,4 +1,5 @@
 var money = 10000;
+var stock1 = "GOOG";
 var stock1Price = getRndInteger(25, 35);
 var stock1Qty = 0;
 
@@ -10,12 +11,14 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var purchaseQTY = prompt("How many shares would you like to buy?", "1");
-
+function purchase(stock){
+var purchaseQty = prompt("How many shares of", stock ," would you like to buy?", "0");
+var purchaseQtyInt = parseInt(purchaseQty)
 //this is concatenating for some reason
 stock1Qty = purchaseQTY + stock1Qty;
-
-
-money = money - (purchaseQTY * stock1Price)
+    
+money = money - (purchaseQtyInt * stock1Price)
 console.log("You currently have $", money)
-console.log("You currently have ", stock1Qty, " shares of stock #1")
+console.log("You currently have ", purchaseQtyInt, " shares of", stock)  
+}
+

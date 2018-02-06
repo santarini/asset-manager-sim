116bdda6e4be0bf944df5cb888a6dc828022fb2b
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-05 20:53:31 -1000

    Update buyShare.js

--- buyShare.js
+++ buyShare.js
@@ -7,16 +7,15 @@ var stockMarketPrice = {GOOG:0, MSFT:0, AMZN:0, BOBA:0};
 var stockMarketQty = {GOOG:0, MSFT:0, AMZN:0, BOBA:0};
 
 //randomize market prices
-
+function marketChange(){
 stockMarketPrice["GOOG"] = getRndInteger(30, 50);
 stockMarketPrice["MSFT"] = getRndInteger(30, 50);
 stockMarketPrice["AMZN"] = getRndInteger(30, 50);
 stockMarketPrice["BOBA"] = getRndInteger(30, 50);
+}
 
 //list your current stats
 console.log("You currently have $", money);
-console.log("Stock 1 currently trades at $",stock1Price);
-console.log("You currently have ",stock1Qty, " shares of stock #1");
 
 //list current market prices
 console.log("GOOG: ", stockMarketPrice["GOOG"]);
@@ -25,20 +24,30 @@ console.log("AMZN: ", stockMarketPrice["AMZN"]);
 console.log("BOBA: ", stockMarketPrice["BOBA"]);
 
 function purchase(stock){
-    var purchaseQty = prompt("How many shares of " + stockMarketPrice[stock] + " would you like to buy?", "0");
+    
+    //dicitonary reference is wrong here, read soemthing about using a map instead
+    var purchaseQty = prompt("How many shares of " + "XXXX" + " would you like to buy?", "0");
     var purchaseQtyInt = parseInt(purchaseQty);
-    console.log(stockMarketPrice[stock]);
-    return stockMarketPrice[stock]
+    
+    if (purchaseQtyInt > 0){
+        stockMarketQty[stock] = purchaseQTY;
+        money = money - (purchaseQTY * stockMarketPrice[stock]);
+        console.log("Purchased " + purchaseQTY +" share of XXXXX at $" + stockMarketPrice[stock] +" each.")
+        console.log("You currently have $", money);
+    }else if (purchaseQtyInt < 0){
+        alert("It doesn't work like that...");
+    }else{
+        alert("Quit wasting my time.");
+    }
 }
 
-    
-    /*
-var purchaseQty = prompt("How many shares of stockMarketPrice[stock] would you like to buy?", "0");
-var purchaseQtyInt = parseInt(purchaseQty)
-stock1Qty = purchaseQTY + stock1Qty;
-    
-money = money - (purchaseQtyInt * stock1Price);
-console.log("You currently have $", money);
-console.log("You currently have ", purchaseQtyInt, " shares of", stock);
-*/
+//view your current holdings
 
+function viewPortfolio(){
+   //list current market prices
+console.log("Your current holdings are:"
+console.log("GOOG: ", stockMarketQty["GOOG"]);
+console.log("MSFT: ", stockMarketQty["MSFT"]);
+console.log("AMZN: ", stockMarketQty["AMZN"]);
+console.log("BOBA: ", stockMarketQty["BOBA"]);
+}

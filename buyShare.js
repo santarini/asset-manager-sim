57d00d5169f07a8ffdbb16605903c76b7fd34f95
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-05 17:37:15 -1000

    Update buyShare.js

--- buyShare.js
+++ buyShare.js
@@ -2,6 +2,10 @@ var money = 10000;
 var stock1Price = getRndInteger(25, 35);
 var stock1Qty;
 
+console.log("You currently have $", money)
+console.log("Stock 1 currently trades at $",stock1Price)
+console.log("You currently have ",stock1QTY, " shares of stock #1")
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -10,6 +14,7 @@ var purchaseQTY = prompt("How many shares would you like to buy?", "1");
 
 
 stock1Qty = purchaseQTY + stock1Qty;
+money = money - (purchaseQTY * stock1Price)
 console.log(stock1Qty)
 
 

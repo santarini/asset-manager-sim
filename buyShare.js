cf97c3defb90b3652981c5ca6a03e85f93e7c3ce
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-05 20:56:04 -1000

    Update buyShare.js

--- buyShare.js
+++ buyShare.js
@@ -12,12 +12,20 @@ stockMarketPrice["GOOG"] = getRndInteger(30, 50);
 stockMarketPrice["MSFT"] = getRndInteger(30, 50);
 stockMarketPrice["AMZN"] = getRndInteger(30, 50);
 stockMarketPrice["BOBA"] = getRndInteger(30, 50);
+console.log("The current market prices are:");
+console.log("GOOG: ", stockMarketPrice["GOOG"]);
+console.log("MSFT: ", stockMarketPrice["MSFT"]);
+console.log("AMZN: ", stockMarketPrice["AMZN"]);
+console.log("BOBA: ", stockMarketPrice["BOBA"]);
 }
+
+marketChange()
 
 //list your current stats
 console.log("You currently have $", money);
 
 //list current market prices
+console.log("The current market prices are:");
 console.log("GOOG: ", stockMarketPrice["GOOG"]);
 console.log("MSFT: ", stockMarketPrice["MSFT"]);
 console.log("AMZN: ", stockMarketPrice["AMZN"]);
@@ -25,7 +33,7 @@ console.log("BOBA: ", stockMarketPrice["BOBA"]);
 
 function purchase(stock){
     
-    //dicitonary reference is wrong here, read soemthing about using a map instead
+    //dicitonary reference is wrong here, read soemthing about using a map instead, need to reference key not value
     var purchaseQty = prompt("How many shares of " + "XXXX" + " would you like to buy?", "0");
     var purchaseQtyInt = parseInt(purchaseQty);
     
@@ -45,7 +53,7 @@ function purchase(stock){
 
 function viewPortfolio(){
    //list current market prices
-console.log("Your current holdings are:"
+console.log("Your current holdings are:");
 console.log("GOOG: ", stockMarketQty["GOOG"]);
 console.log("MSFT: ", stockMarketQty["MSFT"]);
 console.log("AMZN: ", stockMarketQty["AMZN"]);

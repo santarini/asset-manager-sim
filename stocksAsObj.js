0b15f3473dde814bd6ec5de56de32b542141c156
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-06 14:34:48 -1000

    Update stocksAsObj.js

--- stocksAsObj.js
+++ stocksAsObj.js
@@ -1,6 +1,8 @@
 var stocks = {
 MSFT : {ticker: 'MSFT', price: 100, priceChng: 0, prcntChng: 0, lastPrice0: 95, lastPrice1: 90, lastPrice2: 85, lastPrice3: 80, lastPrice4: 75, lastPrice5: 70},
-GOOG : {ticker: 'GOOG', price: 1001, priceChng: 0, prcntChng: 0, lastPrice0: 951, lastPrice1: 901, lastPrice2: 851, lastPrice3: 801, lastPrice4: 751, lastPrice5: 701}
+GOOG : {ticker: 'GOOG', price: 1001, priceChng: 0, prcntChng: 0, lastPrice0: 951, lastPrice1: 901, lastPrice2: 851, lastPrice3: 801, lastPrice4: 751, lastPrice5: 701},
+AMZN : {ticker: 'AMZN', price: 1001, priceChng: 0, prcntChng: 0, lastPrice0: 951, lastPrice1: 901, lastPrice2: 851, lastPrice3: 801, lastPrice4: 751, lastPrice5: 701},
+BOBA : {ticker: 'BOBA', price: 1001, priceChng: 0, prcntChng: 0, lastPrice0: 951, lastPrice1: 901, lastPrice2: 851, lastPrice3: 801, lastPrice4: 751, lastPrice5: 701}
 }
     
 //all stocks

451702cf360551fddba80c87ca7c06ca3fe46198
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-06 14:07:44 -1000

    Create stocksAsObj.js

---
+++ stocksAsObj.js
@@ -0,0 +1,12 @@
+var stocks = {
+MSFT : {ticker: 'MSFT', price: 100, priceChng: 0, prcntChng: 0, lastPrice0: 95, lastPrice1: 90, lastPrice2: 85, lastPrice3: 80, lastPrice4: 75, lastPrice5: 70},
+GOOG : {ticker: 'GOOG', price: 1001, priceChng: 0, prcntChng: 0, lastPrice0: 951, lastPrice1: 901, lastPrice2: 851, lastPrice3: 801, lastPrice4: 751, lastPrice5: 701}
+}
+    
+//all stocks
+var stockMarket = [];
+
+//populate all stock array
+for(var key in stocks) {
+    stockMarket.push(stocks[key]);
+}
